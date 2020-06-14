187e5f1742ac263504f700c6289bfcb5da72d900
higorhms <higorhmscebrac@hotmail.com>
2020-06-14 15:18:52 -0300

    Tests on how to work with a object

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,26 @@ import express from 'express';
 import { buildSchema } from 'graphql';
 import graphqlHTTP from 'express-graphql';
 
+const user = { name: 'Higor', email: 'higor@higor.com', password: '123456' };
+
 const schema = buildSchema(`
   type Query {
+    users: User
+  }
+
+  type User {
     name: String
+    email: String
+    password: String
   }
 `);
 
 const Controller = {
-  name: () => 'aaa',
+  users: {
+    name: () => user.name,
+    email: () => user.email,
+    password: () => user.password,
+  },
 };
 
 const app = express();
